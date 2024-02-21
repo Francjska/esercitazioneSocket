9e3ba0c2e77f3390855673a5e696b265586b140e
Francjska <97961849+Francjska@users.noreply.github.com>
2024-02-21 11:23:10 +0100

    Update Server.java
    Aggiornato il server.java aggiungendo il clienthandler

--- Server.java
+++ Server.java
@@ -1,36 +1,21 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.io.IOException;
 import java.net.*;
-import java.io.*;
+
 public class Server {
-  public static void main(String[] args) throws IOException{
-    
-    int port = 12345;
-    ServerSocket serverSocket = new ServerSocket(port);
-    System.out.println("Server in ascolto sulla porta " + port);
-    try{
-      while(true)
-        {
-          Socket client = serverSocket.accept();
-          PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-          out.println("Ciao client!");
-          client.close();
+    public static void main(String[] args) throws IOException {
+        int port = 12345;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server in ascolto sulla porta " + port);
+
+        try {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connessione accettata da " + clientSocket.getRemoteSocketAddress());
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                new Thread(clientHandler).start();
+            }
+        } finally {
+            serverSocket.close();
         }
     }
-
-    catch(IOException e)
-      {
-        System.out.println("Errore: " + e.getMessage());
-      }
-
-    finally {
-      serverSocket.close();
-    }
-  }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
