323dabc2918044419ca44dc92c6c1119b5a36069
Francjska <97961849+Francjska@users.noreply.github.com>
2024-02-21 11:24:47 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,15 +1,25 @@
-public class Client {
-	public static void main (String args[]) throws IOException{
-		String hostname = "localhost";
-		int port = 12345;
+import java.net.*;
+import java.io.*;
 
-		try (Socket socket = new Socket(hostname, port);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-			String fromserver;
-			while (( fromserver = in.readLine()) != null) {
-				System.out.println("Server: " + fromserver);
-				break;
-			}
-		}
-	}
-}	
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String hostName = "localhost";
+        int port = 12345;
+
+        try (Socket socket = new Socket(hostName, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server: " + in.readLine());
+
+                if (userInput.equalsIgnoreCase("exit")) {
+                    break;
+                }
+            }
+        }
+    }
+}
